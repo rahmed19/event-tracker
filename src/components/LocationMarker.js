6c8a81b062362839df75c6e786610a60f5399031
rahmed19 <ray.ahmed.sec@gmail.com>
2021-04-22 16:27:02 -0700

    Div test

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -8,7 +8,7 @@ export default function LocationMarker({ lat, lng, onClick, eventCategory }) {
     return (
         <div className="location-marker" onClick={() => onClick()} >
             {eventCategory === "wildfires" ?
-                <p>X<Icon onMouseDown={() => onClick()} icon={locationIconWildfire} className="location-icon" /></p> :
+                <p>MR BOO BOO HEAD<Icon onMouseDown={() => onClick()} icon={locationIconWildfire} className="location-icon" /></p> :
                 <Icon onMouseDown={() => onClick()} icon={locationIconVolcano} className="location-icon" />}
         </div>
     )
